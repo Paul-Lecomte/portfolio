98f7feb2d45459f48981ce16839bec47d1088ada
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-24 07:17:02 +0100

    simplified the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,45 +26,9 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-        {/* Layout Structure */}
-        <div className="h-screen flex flex-col">
-            {/* Taskbar */}
-            <div className="taskbar flex items-center justify-between p-4 fixed bottom-0 left-0 right-0">
-                {/* Start Menu Button */}
-                <div className="start-menu text-white">
-                    <div className="start-button w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center">
-                        <span className="text-white text-lg">⚙️</span>
-                    </div>
-                </div>
-
-                {/* Task Icons */}
-                <div className="task-icons flex space-x-4">
-                    <div className="task-icon bg-gray-600 rounded-full p-2 flex items-center justify-center">
-                        <span className="text-white">📂</span>
-                    </div>
-                    <div className="task-icon bg-gray-600 rounded-full p-2 flex items-center justify-center">
-                        <span className="text-white">💻</span>
-                    </div>
-                </div>
-
-                {/* System Tray */}
-                <div className="system-tray flex space-x-4 text-white">
-                    <div className="notification bg-gray-700 p-2 rounded-full">
-                        <span>🔔</span>
-                    </div>
-                    <div className="user-avatar bg-gray-700 p-2 rounded-full">
-                        <span>👤</span>
-                    </div>
-                </div>
-            </div>
-
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
             {/* Desktop (Main Area) */}
-            <div className="desktop-container flex-grow bg-gray-950 relative pt-16">
-                <Desktop />
-            </div>
-        </div>
+            <Desktop />
         </body>
         </html>
     );
