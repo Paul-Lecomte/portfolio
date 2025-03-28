f88360d9b7d14847326b752fdee7ac5f1978af87
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-28 09:50:17 +0100

    removed the close button

--- src/components/WebBrowser.tsx
+++ src/components/WebBrowser.tsx
@@ -8,7 +8,6 @@ const WebBrowser = ({ url, onClose }: any) => {
                 className="w-full h-full rounded-lg"
                 title="Web Browser"
             />
-            <button onClick={onClose} className="absolute top-2 left-2 bg-red-500 text-white p-2 rounded-full">X</button>
         </div>
     );
 };
