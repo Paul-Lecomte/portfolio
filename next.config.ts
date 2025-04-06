22582ba74a7196ca812483bb5d07a77dd591925b
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-06 10:07:51 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
-    devIndicators: false
-    // Additional configurations if needed
+    devIndicators: false,
+    webpack: (config) => {
+        // Resolve Konva to the browser-friendly version
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'konva/lib/index-node': 'konva/lib/index-browser', // Ensures the correct version is used
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
