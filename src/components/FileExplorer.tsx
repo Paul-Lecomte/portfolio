c4d8bdbf2e14f0dd33e4c037467c500bd460e428
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-10 10:49:43 +0200

    working fileexplorer to desktop opening

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -37,7 +37,7 @@ export default function FileExplorer({ onOpenFile }: FileExplorerProps) {
     const handleFileClick = (file: File) => {
         console.log("File clicked:", file); // Log the clicked file
         if (file.type === "file") {
-            onOpenFile(file.name, file.path, file.app); // Pass the file name, path, and app name
+            onOpenFile(file.name, file.url, file.app); // Pass the file name, url, and app name
         }
     };
 
