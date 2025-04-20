ed7cfe9706f99ee6b64011161c613988d7f46ee0
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-20 09:57:10 +0200

    trippy wallpaper

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -17,6 +17,7 @@ import AnimatedWallpaper from "@/components/wallpaper/TestWallpaper";
 import MatrixWallpaper from "@/components/wallpaper/MatrixWallpaper";
 import SublimeWallpaper from "@/components/wallpaper/SolarSystemWallpaper";
 import FractalWallpaper from "@/components/wallpaper/FractalWallpaper";
+import CreativeCodingWallpaper from "@/components/wallpaper/TripyWallpaper"
 import {
     FaFileAlt,
     FaLaptop,
@@ -386,6 +387,8 @@ export default function Desktop() {
                     <SublimeWallpaper />
                 ) : wallpaperType === "matrix" ? (
                     <MatrixWallpaper />
+                ) : wallpaperType === "trippy" ? (
+                    <CreativeCodingWallpaper />
                 ) : wallpaperType === "fractal" ? (
                     <FractalWallpaper />
                 ) : (
@@ -628,7 +631,7 @@ export default function Desktop() {
 
                             {contextMenu.showMoreOptions && (
                                 <div className="absolute left-full top-0 ml-1 bg-white/10 border border-white/20 rounded-xl shadow-xl backdrop-blur-xl z-50 w-48 p-2">
-                                    {["Default", "Solar system", "Matrix", "Fractal"].map((label) => (
+                                    {["Default", "Solar system", "Matrix", "Fractal", "Trippy"].map((label) => (
                                         <button
                                             key={label}
                                             className="text-white text-sm w-full text-left px-3 py-2 hover:bg-white/20 rounded-md transition"
