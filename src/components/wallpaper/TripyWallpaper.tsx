caff555511e6b0133b0f637611f4941b9df99cf2
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-20 10:23:04 +0200

    fixed the sizing

--- src/components/wallpaper/TripyWallpaper.tsx
+++ src/components/wallpaper/TripyWallpaper.tsx
@@ -9,7 +9,7 @@ const CreativeCodingWallpaper: React.FC = () => {
 
         // Set up scene, camera, and renderer
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.z = 1;
 
         const renderer = new THREE.WebGLRenderer({ antialias: true });
