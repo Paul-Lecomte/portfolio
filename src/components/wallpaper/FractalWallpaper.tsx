32948a6baccc239195c2d94dd84eb6ee918295b6
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-20 10:22:02 +0200

    fixed the sizing

--- src/components/wallpaper/FractalWallpaper.tsx
+++ src/components/wallpaper/FractalWallpaper.tsx
@@ -9,7 +9,7 @@ const FractalWallpaper: React.FC = () => {
 
         // Scene setup
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.z = 1;
 
         const renderer = new THREE.WebGLRenderer({ antialias: true });
