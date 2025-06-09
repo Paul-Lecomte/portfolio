063f196ba64992cdc012fc499c99145c0bc0a408
Paul <lecomtepaulvd@gmail.com>
2025-06-09 11:04:29 +0200

    bug fixing

--- src/components/wallpaper/StarNestWallpaper.tsx
+++ src/components/wallpaper/StarNestWallpaper.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 const StarNestWallpaper = () => {
-    const containerRef = useRef(null);
+    const containerRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         if (!containerRef.current) return;
